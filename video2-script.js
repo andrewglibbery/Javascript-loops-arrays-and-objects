00fa1e03623f1ecdb0eb5010f09e91681d415e5c
Andrew Glibbery <andrew.glibbery@gmail.com>
2016-05-15 20:38:38 -0400

    complete do while loops

--- video2-script.js
+++ video2-script.js
@@ -1,9 +1,18 @@
-function randomNumber(upper) {
-  return Math.floor( Math.random() * upper ) + 1;
+var randomNumber = getRandomNumber(10);
+var guess;
+var guessCount = 0;
+var correctGuess = false;
+
+function getRandomNumber(upper) {
+  var num = Math.floor( Math.random() * upper ) + 1;
+  return num;
 }
-var counter = 0;
-while ( counter < 10000 ) {
-  var randNum = randomNumber(6);
-  document.write(randNum + ' ');
-  counter += 1;
-}
+
+do {
+	guess = prompt('I am thinking of a number between 1 and 10. What is it?')
+	guessCount += 1;
+	if (parseInt(guess) === randomNumber) {
+		correctGuess = true
+	}
+} while ( ! correctGuess)
+	document.write('<h1> you guessed the number after </h1> ' + guessCount + ' guesses');
