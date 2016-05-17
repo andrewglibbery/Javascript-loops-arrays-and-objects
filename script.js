eb608b72ffa933c298d02ee670d9768f4e92782c
Andrew Glibbery <andrew.glibbery@gmail.com>
2016-05-17 08:47:12 -0400

    complete challenge 2

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 var html = '';
-var red;
-var green;
-var blue;
 var rgbColor;
 
 function getRandomColor () {
 	var randomColor = Math.floor(Math.random() * 256 );
-	return randomColor
+	return randomColor;
 }
 
-for (var i = 0; i < 10; i += 1) {
+
+
+for (var i = 0; i < 100; i += 1) {
 rgbColor = 'rgb(' + getRandomColor() + ',' + getRandomColor() + ',' + getRandomColor() + ')';
 html += '<div style="background-color:' + rgbColor + '"></div>';
 
